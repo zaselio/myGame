3f99c43d12142523cca7d7ed4ff94e9f446c1ea6
zaselio <88446440+zaselio@users.noreply.github.com>
2022-02-08 00:25:06 +0200

    Add files via upload

---
+++ jScripts/myScript.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function () {
+    $(".about").click(function () {
+        $("#aboutDiv").toggle();
+    });
+
+    $(".howToPlay").click(function () {
+        $("#howToPlayDiv").toggle();
+    });
+
+    $(".closeAbout").click(function () {
+        $("#aboutDiv").hide();
+        $("#gameIframe")[0].contentWindow.focus();
+    });
+
+    $(".closeHowToPlay").click(function () {
+        $("#howToPlayDiv").hide();
+        $("#gameIframe")[0].contentWindow.focus();
+    });
+});
